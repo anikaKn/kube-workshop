73815356f9f97d7e315d0f21a327a8f8bd7b1dc4
Anika Knyshenko <anika.kn.902@gmail.com>
2025-04-07 11:51:37 +0300

    karpenter

--- terraform/hub/main.tf
+++ terraform/hub/main.tf
@@ -29,7 +29,7 @@ provider "helm" {
 provider "kubernetes" {
   host                   = module.eks.cluster_endpoint
   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-  token                  = data.aws_eks_cluster_auth.eks.token
+  # token                  = data.aws_eks_cluster_auth.eks.token
 
   exec {
     api_version = "client.authentication.k8s.io/v1beta1"
